acdb9cc310cb981e26001bffb16b65c9944ff6f4
Juan-Esteban-Trujillo-Montes <159717159+Juan-Esteban-Trujillo-Montes@users.noreply.github.com>
2024-03-22 05:54:29 -1100

    Create TempoCheck.java
    Este es el Main de la aplicación, el ejecutable y desde donde se muestran todas las ventanas de las diferentes secciones de la App

---
+++ TempoCheck.java
@@ -0,0 +1,12 @@
+package com.mycompany.tempocheck;
+import com.mycompany.tempocheck.igu.Principal;
+
+public class TempoCheck {
+
+    public static void main(String[] args) {
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        
+    }
+}
